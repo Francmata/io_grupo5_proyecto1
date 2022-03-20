6183b2d3ee78ecb57ef45ba63d57b45d027a5e2e
AdrianaLopez27 <adrilopezcalderon72@gmail.com>
2022-03-19 19:52:33 -0600

    funciones de abstracción de datos(mejorada y comentada)

--- simplex.py
+++ simplex.py
@@ -1,20 +1,26 @@
 import sys
-
+#funtion 1:main
+#verifies that any parameters other than the name of the
+# program have been entered
 def main():
     if len(sys.argv)<2:
-        print("debe introducir el nombre del archivo")
+        print("must have introduce the name of the file too")
         sys.exit()
     else:
-        abstractData(sys.argv)
-        
-def abstractData(input):
+        abstractImputData(sys.argv)
+
+#funtion 2: abstractImputData
+#obtein the name of the txt file
+def abstractImputData(input):
     filename= input[1]
-    file1=open(filename,"r")
-    file2=file1.readlines()
-    file1.close()
-    convertData (file2)
-   
-def convertData(list):
+    abstractData(filename)
+
+#funtion 3: abstractImputData
+# convert the txt information into
+# a list with coefficients for the objective function
+# a matrix with the restricctions
+#and the values of the method, optimization type, number of decision variables, number of restrictions
+def abstractData(file):
     problemMatrix=[]
     method=0
     optimization=""
@@ -22,39 +28,38 @@ def convertData(list):
     restrictionNum= 0
     objetiveFuction=[]
     count= 0
-
-    while list!=[]:
-        if count >=2:
-            row=list[0].rstrip()
-            row1=row.rstrip(",")
-            sub=row1.split(",")
-            problemMatrix=problemMatrix+[sub]
-            count+=1
-            list=list[1:]
-        
-        elif count ==0:
-            row=list[0].rstrip()
-            row1=row.rstrip(",")
-            sub=row1.split(",")
-            method=int(sub[0])
-            optimization= sub[1]
-            descitionVar = int(sub[2])
-            restrictionNum= int(sub[3])
-            count+=1
-            list=list[1:]
-        else:
-            row=list[0].rstrip()
-            row1=row.rstrip(",")
-            sub=row1.split(",")
-            objetiveFuction=sub
-            count+=1
-            list=list[1:]
+    with open (file) as fileObject:
+        for row  in fileObject:
+            if count >=2:
+                row1=row.rstrip()
+                sub=row1.split(",")
+                problemMatrix=problemMatrix+[sub]
+                count+=1
+            elif count ==0:
+                sub=row.split(",")
+                method=int(sub[0])
+                optimization= sub[1]
+                descitionVar = int(sub[2])
+                restrictionNum= int(sub[3])
+                count+=1
+            else:
+                row1=row.rstrip()
+                sub=row1.split(",")
+                objetiveFuction=sub
+                count+=1
+ #examples of format:
+ # objetiveFuction: ['3', '5'] 
+ # resticction Matrix [['2', '1', '<=', '6'], ['-1', '3', '<=', '9'], ['0', '1', '<=', '4']]    
+ # method number: 0 
+ # optimization type: max  
+ # number of restrictions: 3
+ # number of decision variables: 2  
     print ("objetiveFuction",objetiveFuction)
     print ("problemMatrix",problemMatrix)
     print (" method", method)
     print (" optimization", optimization)
     print (" restrictionNum", restrictionNum)
     print (" descitionVar", descitionVar)
-
 main()
+
 
